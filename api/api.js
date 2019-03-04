e74bb67cc9931e153c096a1b74ed31fa1a1e4da5
Michael Lin <mlin4589@gmail.com>
2019-03-03 22:43:28 -0500

    Started work on server

--- api/api.js
+++ api/api.js
@@ -1,7 +1,36 @@
 const express = require('express')
+
 const app = express()
 const port = 8080
 
+// Initialize firebase SDK
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://transparency-c26c5.firebaseio.com'
+});
+
+
+// Authentication Service
+const auth = admin.auth();
+
+// DB service
+const db = admin.firestore();
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+// bind endpoints to queries
+
+app.get('/chores', (req, res) => {
+    var choresRef = db.collection("chores");
+    var queryRef = choresRef.get().then(snapshot => {
+        snapshot.forEach(doc => {
+            console.log(doc.id, '=>', doc.data());
+        });
+        res.send('Test!');
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
