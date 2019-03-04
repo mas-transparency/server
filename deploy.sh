ceb6969cd4a2dccf176e06c8e743d403d8a0c7e2
Michael Lin <mlin4589@gmail.com>
2019-03-03 23:19:42 -0500

    Ignore node_modules

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-rsync -r . ubuntu@3.93.95.228:~/server --delete
+rsync -r --exclude=node_modules . ubuntu@3.93.95.228:~/server --delete
 ssh ubuntu@3.93.95.228 'cd ~/server && sudo docker-compose -f docker-compose.yml down && sudo docker-compose -f docker-compose.yml up -d'
