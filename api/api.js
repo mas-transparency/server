bc7ab7437205465c4d68d9bbcd4e4c5528157e7d
Michael Lin <mlin4589@gmail.com>
2019-04-04 20:27:16 -0400

    Fixed notifications across multiple expo apps

--- api/api.js
+++ api/api.js
@@ -2,6 +2,7 @@ const { Expo } = require('expo-server-sdk');
 const expo = new Expo();
 const express = require('express')
 const bodyParser = require('body-parser')
+const rp = require('request-promise')
 const { check, validationResult } = require('express-validator/check');
 const app = express()
 
@@ -314,7 +315,7 @@ app.post('/notify',[
     var devicesRef = db.collection("devices");
     var queryRef = devicesRef.get().then(snapshot => {
         var tokens = [];
-        var messages = [];
+        var promises = [];
         snapshot.forEach(doc => {
             tokens.push(doc.data().idToken);
         })
@@ -328,15 +329,29 @@ app.post('/notify',[
           }
 
           // Construct a message (see https://docs.expo.io/versions/latest/guides/push-notifications.html)
-          messages.push({
-            to: pushToken,
-            sound: 'default',
-            body: req.body.body,
-            data: { body: req.body.body, title: req.body.title },
-          })
+          promises.push(
+              rp.post({
+                url: 'https://exp.host/--/api/v2/push/send',
+                headers: {
+                  'Accept': 'application/json',
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    to: pushToken,
+                    sound: 'default',
+                    body: req.body.body,
+                    data: { body: req.body.body, title: req.body.title}
+                })
+            })
+          )
         }
-        sendMessages(messages);
-        res.json({"status": "success"});
+        return Promise.all(promises);
+    }).then(response => {
+        res.status(200).json(response);
+        console.log(response);
+    }).catch(error => {
+        console.log(error);
+        res.status(401).json(error);
     });
 
 
@@ -355,69 +370,5 @@ app.get('/assigned-chores', (req, res) => {
         res.json(response);
     });
 });
-
-
-// sends a bunch of expo messages
-async function sendMessages(messages) {
-    let chunks = expo.chunkPushNotifications(messages);
-    let tickets = [];
-    (async () => {
-      // Send the chunks to the Expo push notification service. There are
-      // different strategies you could use. A simple one is to send one chunk at a
-      // time, which nicely spreads the load out over time:
-      for (let chunk of chunks) {
-        try {
-          let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-          console.log(ticketChunk);
-          tickets.push(...ticketChunk);
-          // NOTE: If a ticket contains an error code in ticket.details.error, you
-          // must handle it appropriately. The error codes are listed in the Expo
-          // documentation:
-          // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    })();
-
-    let receiptIds = [];
-    for (let ticket of tickets) {
-      // NOTE: Not all tickets have IDs; for example, tickets for notifications
-      // that could not be enqueued will have error information and no receipt ID.
-      if (ticket.id) {
-        receiptIds.push(ticket.id);
-      }
-    }
-
-    let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
-    (async () => {
-      // Like sending notifications, there are different strategies you could use
-      // to retrieve batches of receipts from the Expo service.
-      for (let chunk of receiptIdChunks) {
-        try {
-          let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
-          console.log(receipts);
-
-          // The receipts specify whether Apple or Google successfully received the
-          // notification and information about an error, if one occurred.
-          for (let receipt of receipts) {
-            if (receipt.status === 'ok') {
-              continue;
-            } else if (receipt.status === 'error') {
-              console.error(`There was an error sending a notification: ${receipt.message}`);
-              if (receipt.details && receipt.details.error) {
-                // The error codes are listed in the Expo documentation:
-                // https://docs.expo.io/versions/latest/guides/push-notifications#response-format
-                // You must handle the errors appropriately.
-                console.error(`The error code is ${receipt.details.error}`);
-              }
-            }
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    })();
-}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
